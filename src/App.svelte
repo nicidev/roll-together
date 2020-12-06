7ddb7009eda1336bf74f62a3fbcac64ad29b000f
Nicole Heinze <neinz@posteo.de>
2020-12-06 14:28:51 +0100

    setup project

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+//	export let name;
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+	import Button from './shared/Button.svelte';
+	import Die from './components/Die.svelte';
+	import Tab from './components/Tabs.svelte';
+
+	$: currentRoll = 6;
+	const handleRoll = () => {
+		currentRoll = Math.floor(Math.random() * 6) + 1;
+		console.log(currentRoll);
+	}
+
+	let playerName = '';
+
+</script>
+<Header/>
+
+<main>
+	<div>
+		<label for="playerName">Player name</label>
+		<input type="text" id="playerName" bind:value={playerName}>
+	</div>
+
+
+	<div class="result">
+		<Die numberRolled={currentRoll}  on:click={() => handleRoll()}/>
+	</div>
+</main>
+
+<Footer/>
+<style>
+	main {
+		text-align: center;
+		padding: 5em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+</style>
---
+++ src/components/Die.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let numberRolled;
+    $: imageNumber = numberRolled || 6
+</script>
+
+<div class="die" on:click>
+    <img src="img/{imageNumber}.gif" alt={imageNumber}>
+</div>
+
+<style>
+
+</style>
